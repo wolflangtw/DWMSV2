ba7afae2e5c0bd6c562fb89c1cee4742314903cf
SpeditAt <119734957+SpeditAt@users.noreply.github.com>
2023-05-24 19:24:46 +0800

    1

---
+++ index.js
@@ -0,0 +1,20 @@
+// 初始化
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
