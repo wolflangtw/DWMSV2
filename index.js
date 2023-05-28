987d0b34c1118e582f0166fa2fe1ce0b18ffed1d
SpeditAt <119734957+SpeditAt@users.noreply.github.com>
2023-05-28 10:51:19 +0800

    1

--- index.js
+++ index.js
@@ -18,3 +18,4 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-scrollbar',
     },
 });
+
--- map/index.js
+++
@@ -1,9 +0,0 @@
-$("#load img").img(function() {  
-
-    //圖片預設隱藏  
-    $(this).hide();  
-
-     //使用fadeIn特效  
-    $(this).fadeIn("5000"); 
-
-})
